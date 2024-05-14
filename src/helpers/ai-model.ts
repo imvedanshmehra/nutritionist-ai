96c73f076d85bb8ee33aeb233e34ad08d218f0ef
Vedansh Mehra <vedanshofficial@gmail.com>
2024-05-14 11:47:11 +0530

    update gpt model to 4o

--- src/helpers/ai-model.ts
+++ src/helpers/ai-model.ts
@@ -12,8 +12,8 @@ const modelConfig = {
   presence_penalty: 0,
 };
 
-const defaultModel = "gpt-3.5-turbo";
-const turboModel = "gpt-4-turbo";
+const defaultModel = "gpt-4o";
+const turboModel = "gpt-4o";
 
 const systemPrompt =
   "Act as a AI nutritionist, you are developed to help people choose healthier habits by telling them what is healthy for them and what is not. You will be provided a list of ingredients used in a food product or an image of the ingredients label on the packaged food and you have to tell, how good/bad it is and what's it's impact on the health as a certified nutritionist would tell. Please respond in a friendly manner with a human like tone and use emojis in your responses. You can cross question when necessary. Politely reject the questions outside the scope of health, nutrition and general fitness.";
